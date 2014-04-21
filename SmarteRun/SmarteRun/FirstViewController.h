63a1fb8b024226c32ef21011093c3a3628e2828c
Buffalo Hird <buffalohird@college.harvard.edu>
2014-04-20 21:45:12 -0400

    added dat code

--- SmarteRun/SmarteRun/FirstViewController.h
+++ SmarteRun/SmarteRun/FirstViewController.h
@@ -1,13 +1,36 @@
-//
-//  FirstViewController.h
-//  SmarteRun
-//
-//  Created by Buffalo Hird on 4/15/14.
-//  Copyright (c) 2014 Buffalo Hird. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
+#import <CoreMotion/CoreMotion.h>
+
+
+double currentMaxAccelX;
+double currentMaxAccelY;
+double currentMaxAccelZ;
+double currentMaxRotX;
+double currentMaxRotY;
+double currentMaxRotZ;
 
 @interface FirstViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UILabel *accX;
+@property (strong, nonatomic) IBOutlet UILabel *accY;
+@property (strong, nonatomic) IBOutlet UILabel *accZ;
+
+@property (strong, nonatomic) IBOutlet UILabel *maxAccX;
+@property (strong, nonatomic) IBOutlet UILabel *maxAccY;
+@property (strong, nonatomic) IBOutlet UILabel *maxAccZ;
+
+@property (strong, nonatomic) IBOutlet UILabel *rotX;
+@property (strong, nonatomic) IBOutlet UILabel *rotY;
+@property (strong, nonatomic) IBOutlet UILabel *rotZ;
+
+@property (strong, nonatomic) IBOutlet UILabel *maxRotX;
+@property (strong, nonatomic) IBOutlet UILabel *maxRotY;
+@property (strong, nonatomic) IBOutlet UILabel *maxRotZ;
+
+
+
+
+- (IBAction)resetMaxValues:(id)sender;
+
+@property (strong, nonatomic) CMMotionManager *motionManager;
 
 @end
